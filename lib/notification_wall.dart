2ba05e0a6e9fbbd2d4a64a12b907700b45d48b5b
Lauro C Oliveira <lauro@hostcert.com.br>
2021-04-15 10:33:40 -0300

    added Firebase and bg handler

--- lib/notification_wall.dart
+++ lib/notification_wall.dart
@@ -1,7 +1,103 @@
+/*
+ * *
+ *  * Created by Lauro Cesar de Oliveira <lauro@hostcert.com.br> on 4/15/21 10:32 AM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *  * Last modified 4/15/21 10:15 AM
+ */
+
 library notification_wall;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.high,
+);
+
+class NotificationWall extends StatefulWidget {
+  final Widget onNewNotification; // I will  push a new route with this widget
+  Widget? onSettingUpWall; // I will return this while setting up notifications wall
+  final Widget childWidget; // I will return this after proper setting up notification wall
+
+  NotificationWall({required this.onNewNotification, required this.childWidget, this.onSettingUpWall});
+
+  @override
+  _NotificationWallState createState() => _NotificationWallState();
+}
+
+class _NotificationWallState extends State<NotificationWall> {
+  bool isReady = false;
+
+  Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    // await Firebase.initializeApp();
+    print('Handling a background message ${message.messageId}');
+  }
+
+  Future<void> onInitWall() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+    }
+
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    onInitWall().then((value) => {
+          FirebaseMessaging.onMessage.listen((RemoteMessage? message) {
+            RemoteNotification? notification = message?.notification;
+            AndroidNotification? android = message?.notification?.android;
+            if (notification != null && android != null) {
+              flutterLocalNotificationsPlugin.show(
+                  notification.hashCode,
+                  notification.title,
+                  notification.body,
+                  NotificationDetails(
+                    android: AndroidNotificationDetails(
+                      channel.id,
+                      channel.name,
+                      channel.description,
+                      icon: 'launch_background',
+                    ),
+                  ));
+            }
+          }),
+          FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) {
+            print('A new onMessageOpenedApp event was published!');
+          }),
+          FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {
+            print(message);
+            // Navigator.pushNamed(context, '/message', arguments: MessageArguments(message, true));
+          }),
+          setState(() {
+            isReady = true;
+          })
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
--- test/notification_wall_test.dart
+++ test/notification_wall_test.dart
@@ -1,12 +1,15 @@
+/*
+ * *
+ *  * Created by Lauro Cesar de Oliveira <lauro@hostcert.com.br> on 4/15/21 10:32 AM
+ *  * Copyright (c) 2021 . All rights reserved.
+ *  * Last modified 4/15/21 9:45 AM
+ *
+ */
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:notification_wall/notification_wall.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  test('adds one to input values', () {});
 }
